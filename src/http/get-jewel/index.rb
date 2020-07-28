6dd7cb656c26989af361bfd4ec2e606f8569448c
Paul Chin Jr <pchinjr@gmail.com>
2020-07-28 11:32:54 -0400

    remove bundler

--- src/http/get-jewel/index.rb
+++ src/http/get-jewel/index.rb
@@ -1,12 +1,4 @@
-require 'json'
-require 'architect/functions'
-
 def handler(request)
-  {body: JSON.generate(Arc.reflect)}
+  html = '<b>Hello world from Ruby!</b>'
+  {headers: {'content-type': 'text/html; charset=utf-8;'}, body: html}
 end
-
-
-# def handler(request)
-#   html = '<b>Hello world from Ruby!</b>'
-#   {headers: {'content-type': 'text/html; charset=utf-8;'}, body: html}
-# end
